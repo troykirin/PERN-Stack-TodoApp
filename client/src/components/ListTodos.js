810c32883fd1717cb0b587404b628c649636813d
Troy <troykirin@gmail.com>
2020-06-12 09:11:01 -0700

    bootstrap 4 table list

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const ListTodos = () => {};
+const ListTodos = () => {
+  return (
+    <Fragment>
+      {" "}
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/*    <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td> */}
+          </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
 
 export default ListTodos;
