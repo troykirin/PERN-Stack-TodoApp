1d6e0b20a0da7751cc120c4bc461efa5f2c1cda5
Troy <troykirin@gmail.com>
2020-06-12 09:17:01 -0700

    getTodos returns json data to console

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,6 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
+  //create state
+  const [todos, setTodos] = useState([]); // default val empty array
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos"); //by default fetch is a get reqeust
+      const jsonData = await response.json(); // parse the json data
+
+      setTodos(jsonData); //setTodos only way to change state
+
+      console.log(jsonData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  });
   return (
     <Fragment>
       {" "}
