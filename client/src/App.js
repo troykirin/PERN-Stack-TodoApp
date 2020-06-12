fe36bca011c2a04c4d71b19fa0bc45cc04d8bb01
Troy <troykirin@gmail.com>
2020-06-12 09:01:45 -0700

    inputTodo done, add tasks via React Interface

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import InputTodo from "./components/inputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
